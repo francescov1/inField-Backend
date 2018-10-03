7429974f0aa4f44b815a96f4bcae017263e5bc06
Francesco Virga <francescod.virga@gmail.com>
2018-10-03 14:04:47 -0400

    Test logs aws

--- controllers/authenticationController.js
+++ controllers/authenticationController.js
@@ -8,6 +8,8 @@ module.exports = {
   // this should be developed into a professional auth system. Current auth fine for testing and MVP purposes
 
   apiAuth: function(req, res, next) {
+    console.log('------ CONFIG ------')
+    console.log(config)
     const authToken = req.headers['auth_token'];
     if (!authToken || authToken !== config.auth_token)
       throw new UnauthorizedError('Authentication token required');
