1573561f0b38868db1702d3535511278add47500
Francesco Virga <francescod.virga@gmail.com>
2018-10-03 14:36:23 -0400

    Remove logs

--- controllers/authenticationController.js
+++ controllers/authenticationController.js
@@ -8,8 +8,6 @@ module.exports = {
   // this should be developed into a professional auth system. Current auth fine for testing and MVP purposes
 
   apiAuth: function(req, res, next) {
-    console.log('------ HEADERS ------')
-    console.log(req.headers)
     const authToken = req.headers['auth-token'];
     if (!authToken || authToken !== config.auth_token)
       throw new UnauthorizedError('Authentication token required');
