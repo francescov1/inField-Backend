fa5d3310056caccc35efb3e45d41279949ce8bec
Francesco Virga <francescod.virga@gmail.com>
2018-10-03 14:21:21 -0400

    Add cors

--- controllers/authenticationController.js
+++ controllers/authenticationController.js
@@ -8,9 +8,8 @@ module.exports = {
   // this should be developed into a professional auth system. Current auth fine for testing and MVP purposes
 
   apiAuth: function(req, res, next) {
-    console.log('------ CONFIG ------')
-    console.log(config.auth_token)
-    console.log(req.headers['auth_token'])
+    console.log('------ HEADERS ------')
+    console.log(req.headers)
     const authToken = req.headers['auth_token'];
     if (!authToken || authToken !== config.auth_token)
       throw new UnauthorizedError('Authentication token required');
